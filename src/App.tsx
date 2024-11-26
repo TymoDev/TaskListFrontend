3142ba1c54758f86b7fa932e850595f6cfd11664
CollectorsQ <tymofiy.dol@gmail.com>
2024-11-26 13:33:02 +0200

    Moved the logic to the hook component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { FormEvent, useEffect, useState } from "react";
 import Container from "./components/Container";
 import Input from "./components/Input";
 import Summary from "./components/Summary/Summary";
 import Tasks from "./components/Tasks/Tasks";
-import { fetchTasks, createTask, updateTask, deleteTask } from "./components/Tasks/TaskRequestHttp";
-import { v4 as uuidv4 } from "uuid";
+import useTasks from "./Hooks/useTasks";
 
 export interface Task {
   name: string;
@@ -13,73 +11,7 @@ export interface Task {
 }
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-    const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  // Load task
-  useEffect(() => {
-    const loadTasks = async () => {
-      setLoading(true);
-      try {
-        const data = await fetchTasks();
-        setTasks(data);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    loadTasks();
-  }, []);
-  
-  // Add task
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>, value: string) => {
-    e.preventDefault();
-    setError(null);
-  
-    const guid = uuidv4();
-  
-    try {
-      await createTask({ guid, taskName: value, taskStatus: "pending" });
-      const updatedTasks = await fetchTasks(); 
-      setTasks(updatedTasks); 
-    } catch (err: any) {
-      setError(err.message);
-    }
-  };
-  
-  
-
-  // Update task
-  const toggleDoneTask = async (id: string, done: string) => {
-    const taskToUpdate = tasks.find((task) => task.id === id); 
-   if (!taskToUpdate) return;
-  
-    try {
-      const newStatus = done === "done" ? "pending" : "done"; 
-      await updateTask(id, taskToUpdate.name, { done: newStatus });
-  
-      setTasks((prevTasks) =>
-        prevTasks.map((task) =>
-          task.id === id ? { ...task, done: newStatus } : task
-        )
-      );
-    } catch (err: any) {
-      setError(err.message);
-    }
-  };
-  
-
-  // Delete task
-  const handleDeleteTask = async (id: string) => {
-    try {
-      await deleteTask(id);
-      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
-    } catch (err: any) {
-      setError(err.message);
-    }
-  };
+  const { tasks, loading, error, addTask, toggleTaskStatus, deleteTaskById } = useTasks();
 
   return (
     <div className="flex justify-center m-5">
@@ -95,13 +27,13 @@ function App() {
             )}
           </Container>
           <Container>
-            <Input handleSubmit={handleSubmit} />
+            <Input handleSubmit={addTask} />
           </Container>
           <Container title={"Tasks"}>
             <Tasks
               tasks={tasks}
-              toggleDone={toggleDoneTask}
-              handleDelete={handleDeleteTask}
+              toggleDone={toggleTaskStatus}
+              handleDelete={deleteTaskById}
             />
           </Container>
         </div>
---
+++ src/Hooks/useTasks.tsx
@@ -0,0 +1,80 @@
+import { useState, useEffect, FormEvent } from "react";
+import { fetchTasks, createTask, updateTask, deleteTask } from "../../src/components/Tasks/TaskRequestHttp";
+import { v4 as uuidv4 } from "uuid";
+
+export interface Task {
+  name: string;
+  done: string;
+  id: string;
+}
+
+export const useTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  // Fetch tasks
+  useEffect(() => {
+    const loadTasks = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchTasks();
+        setTasks(data);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadTasks();
+  }, []);
+
+  // Add task
+  const addTask = async (e: FormEvent<HTMLFormElement>, value: string) => {
+    e.preventDefault();
+    setError(null);
+  
+    const guid = uuidv4();
+  
+    try {
+      await createTask({ guid, taskName: value, taskStatus: "pending" });
+      const updatedTasks = await fetchTasks(); 
+      setTasks(updatedTasks); 
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  // Update task
+  const toggleTaskStatus = async (id: string, done: string) => {
+    const taskToUpdate = tasks.find((task) => task.id === id);
+    if (!taskToUpdate) return;
+
+    try {
+      const newStatus = done === "done" ? "pending" : "done";
+      await updateTask(id, taskToUpdate.name, { done: newStatus });
+
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === id ? { ...task, done: newStatus } : task
+        )
+      );
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  // Delete task
+  const deleteTaskById = async (id: string) => {
+    try {
+      await deleteTask(id);
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  return { tasks, loading, error, addTask, toggleTaskStatus, deleteTaskById };
+};
+
+export default useTasks;
